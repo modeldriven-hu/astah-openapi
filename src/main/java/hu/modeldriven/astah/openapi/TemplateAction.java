7c659c726214bc444d1bc6da5f980656923e3287
sz332 <zsolt.sandor@gmail.com>
2024-05-08 09:46:46 +0200

    Added file opening dialog

--- src/main/java/hu/modeldriven/astah/openapi/TemplateAction.java
+++ src/main/java/hu/modeldriven/astah/openapi/TemplateAction.java
@@ -11,21 +11,34 @@ import hu.modeldriven.openapi.OpenAPIObject;
 import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 
+import java.io.File;
+
 public class TemplateAction implements IPluginActionDelegate {
 
 	public Object run(IWindow window) throws UnExpectedException {
 		JOptionPane.showMessageDialog(null, "Starting creating of openapi elements");
 
-		OpenAPIParser parser = new OpenAPIParser();
+		var fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
 
-		SwaggerParseResult result = parser.readLocation("https://petstore3.swagger.io/api/v3/openapi.json", null, null);
-		OpenAPIObject openAPIObject = new OpenAPIObject(result.getOpenAPI());
-		try {
-			AstahRepresentation representation = new AstahRepresentation();
-			var targetPackage = representation.findPackage("api");
-			openAPIObject.build(new BuildInstruction(targetPackage));
-		} catch (ModelBuildingException e) {
-			JOptionPane.showMessageDialog(null, e.getMessage());
+		int fileSelectionResult = fileChooser.showOpenDialog(window.getParent());
+
+		if (fileSelectionResult == JFileChooser.APPROVE_OPTION) {
+			var selectedFile = fileChooser.getSelectedFile();
+
+			JOptionPane.showMessageDialog(null, "Reading file: " + selectedFile.getAbsolutePath());
+
+			OpenAPIParser parser = new OpenAPIParser();
+			SwaggerParseResult result = parser.readLocation(selectedFile.getAbsolutePath(), null, null);
+			OpenAPIObject openAPIObject = new OpenAPIObject(result.getOpenAPI());
+			try {
+				AstahRepresentation representation = new AstahRepresentation();
+				var targetPackage = representation.findPackage("api");
+				openAPIObject.build(new BuildInstruction(targetPackage));
+			} catch (ModelBuildingException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+
 		}
         return null;
     }
